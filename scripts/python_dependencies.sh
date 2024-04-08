691291350a25bae8a88e69186e55b4e2291701bc
Vinay23107 <157478658+Vinay23107@users.noreply.github.com>
2024-04-09 02:10:04 +0530

    Update python_dependencies.sh

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -3,3 +3,5 @@
 virtualenv /home/ubuntu/env
 source /home/ubuntu/env/bin/activate
 pip install -r /home/ubuntu/vacation/requirements.txt
+sudo chmod 777 /home/ubuntu/vacation
+sudo chmod 777 /home/ubuntu/vacation/db.sqlite3
